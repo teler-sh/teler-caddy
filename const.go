6479fa34645fbdc3b580297c220060eb470d6547
Dwi Siswanto <git@dw1.io>
2024-06-20 20:58:41 +0700

    refactor: update module ID
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- const.go
+++ const.go
@@ -3,7 +3,7 @@ package teler
 const (
 	// dir stands for directive (name)
 	dir      = "teler_waf"
-	moduleID = "http.handlers.waf"
+	moduleID = "http.handlers.teler"
 
 	json = "json"
 	yaml = "yaml"
